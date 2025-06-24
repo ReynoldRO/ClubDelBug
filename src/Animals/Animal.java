7bd5f3b28e5fb6db6c659902bb95cfbbb795d920
GipsyAgui <gipsyaguirre25@gmail.com>
2025-06-24 16:24:52 -0600

    Se creo la clase Animal y se modifico el paquete animal a animals

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Animal;
+package Animals;
 import Utils.UtilDate;
 import java.time.LocalDate;
 
