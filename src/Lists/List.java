32e74a0bf062269b893f99bb6bd966faf6abee19
Reynold <rruizo@est.utn.ac.cr>
2025-06-27 13:54:49 -0600

    Creacion de clase lists
    Se crea el paquete lists y la clase list

---
+++ src/Lists/List.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Student
+ */
+
+public interface List <T> {
+    
+    public T find (Object id);
+    
+}
